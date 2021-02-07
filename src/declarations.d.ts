233b6c5187930b88bf4f5f646e4b5bb4887ead55
Borut Balazek <bobalazek124@gmail.com>
2021-02-07 09:38:23 +0100

    Create declarations.d.ts

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.env';
+declare module '*.dds';
+declare module '*.hdr';
+declare module '*.glb';
+declare module '*.gltf';
+declare module '*.stl';
